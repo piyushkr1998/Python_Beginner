4db8e82a4d434048be6c48f8a562325a793eb5d4
Piyush Kumar <piyushkr6722@gmail.com>
2026-02-23 12:37:39 +0530

    First Python Code

---
+++ Experiment 1/Q12.py
@@ -0,0 +1,5 @@
+a = 1
+b = 0
+
+print("a b  a&b  a|b  a^b")
+print(a, b, a & b, a | b, a ^ b)
---
+++ Experiment 1/Q4.py
@@ -0,0 +1,9 @@
+a = 10          
+b = 5.5         
+c = "Python"   
+d = True        
+
+print(a)
+print(b)
+print(c)
+print(d)
