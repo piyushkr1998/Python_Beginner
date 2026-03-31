4f657f89b2b35addde8a94c19fdec65d582d0522
Piyush Kumar <piyushkr6722@gmail.com>
2026-03-31 23:03:54 +0530

    Basic python Code

---
+++ Experiment2/Q3.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the first number :"))
+m=int(input("Enter the second number :"))
+
+if (n > m):
+    print("The first number is greater ",n)
+
+elif (m > n):
+    print("The second number is greater ",m)
+
+else:
+    print("No number is greater ")    
