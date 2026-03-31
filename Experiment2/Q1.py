b3810380c763a27623ce76f7368b9a62fb88f7bf
Piyush Kumar <piyushkr6722@gmail.com>
2026-03-31 23:00:54 +0530

    Basic python code

---
+++ Experiment2/Q1.py
@@ -0,0 +1,7 @@
+n=int(input("Enter any number :"))
+
+if (n % 3 == 0 and n % 5 == 0 ):
+    print("the number is divisible by them", n)
+
+else :
+        print("The number is not divisible by 3,5")
