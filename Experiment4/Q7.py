d819358035cc8421e7d859d6136a65c7a58159ff
Piyush Kumar <piyushkr6722@gmail.com>
2026-04-10 20:24:28 +0530

    Python Code

---
+++ Experiment4/Q7.py
@@ -0,0 +1,18 @@
+n = int(input("Enter number of fruits in each set: "))
+s1 = set()
+s2 = set()
+print("\nEnter fruits for Set 1:")
+for i in range(n):
+    fruit = input()
+    s1.add(fruit)
+print("\nEnter fruits for Set 2:")
+for i in range(n):
+    fruit = input()
+    s2.add(fruit)
+common = s1.intersection(s2)
+only_s1 = s1.difference(s2)
+all_fruits = s1.union(s2)
+total_count = len(all_fruits)
+print("\nFruits in both sets:", common)
+print("Fruits only in Set 1:", only_s1)
+print("Total count of unique fruits from both sets:", total_count)
