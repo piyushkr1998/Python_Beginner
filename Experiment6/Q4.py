76997b8dc832849aeb8dddc2c584303702d1cd45
Piyush Kumar <piyushkr6722@gmail.com>
2026-04-24 19:55:42 +0530

    Python Code

--- Experiment6/Q4.py
+++ Experiment6/Q4.py
@@ -1,39 +1,7 @@
-# Input number of persons
-n = int(input("Enter number of persons: "))
+def fibo(n,a=0,b=1):
+    if (n==0):
+        return 
+    print(a)
 
-# Create dictionary
-students = {}
-
-# Taking input
-for i in range(n):
-    name = input("Enter name: ")
-    city = input("Enter city: ")
-    students[name] = city
-
-# a) Display all names
-print("\nAll Names:")
-for name in students.keys():
-    print(name)
-
-# b) Display all cities
-print("\nAll Cities:")
-for city in students.values():
-    print(city)
-
-# c) Display student name and city
-print("\nStudent Name and City:")
-for name, city in students.items():
-    print(name, "->", city)
-
-# d) Count number of students in each city
-city_count = {}
-
-for city in students.values():
-    if city in city_count:
-        city_count[city] += 1
-    else:
-        city_count[city] = 1
-
-print("\nNumber of students in each city:")
-for city, count in city_count.items():
-    print(city, ":", count)
+    fibo(n-1, b ,a+b)
+fibo(5)     
